5345a8c145dc71372db301060866a03481731d89
Ilya Koshelev <mifomen@yandex.ru>
2018-03-26 12:53:56 +0300

    change header and start coding main block

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,1 @@
-console.log("mifomen"),console.log("mifomen2");
+var nojs=document.querySelector(".main-menu-nojs"),ButtonOfMenu=document.querySelector(".main-menu__toggle"),MainMenu=document.querySelector(".main-menu"),nav=document.querySelector(".main-menu__list");document.addEventListener("DOMContentLoaded",function(e){MainMenu.classList.remove("main-menu--nojs")}),ButtonOfMenu.addEventListener("click",function(e){e.preventDefault(),MainMenu.classList.toggle("main-menu--opened"),MainMenu.classList.toggle("main-menu--closed")}),console.log("mifomen");
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
-console.log("mifomen")
 
-console.log("mifomen2")
+var nojs=document.querySelector(".main-menu-nojs"),
+    ButtonOfMenu=document.querySelector(".main-menu__toggle"),
+    MainMenu=document.querySelector(".main-menu"),
+    nav=document.querySelector(".main-menu__list");
+
+  // nav.classList.remove('menu-nav--opened');
+  // nav.classList.remove('menu-nav--nojs');
+  // nav.classList.add('menu-nav--closed');
+
+document.addEventListener("DOMContentLoaded",function(a){
+ MainMenu.classList.remove("main-menu--nojs")
+});
+
+ButtonOfMenu.addEventListener("click",function(evt){
+   evt.preventDefault(),
+   MainMenu.classList.toggle("main-menu--opened"),
+   MainMenu.classList.toggle("main-menu--closed")
+});
+console.log('mifomen')
